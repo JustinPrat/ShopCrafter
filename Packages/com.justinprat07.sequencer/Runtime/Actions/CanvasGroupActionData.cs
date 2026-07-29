3d33589d588b3a7efe9d14b22649450d3b21b8a4
JustinPrat <justinprat07@gmail.com>
2026-07-29 11:57:28 +0200

    [PROG] Sequencer update

---
+++ Packages/com.justinprat07.sequencer/Runtime/Actions/CanvasGroupActionData.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using UnityEngine;
+
+namespace Sequencer.Actions
+{
+    [CreateAssetMenu(fileName = "CanvasGroupActionData", menuName = "Sequencer/CanvasGroupActionData")]
+    public class CanvasGroupActionData : SequenceActionData
+    {
+        [Range(0f, 1f)]
+        public float TargetValue = 1;
+
+        public float Duration = 1;
+        public AnimationCurve Curve = AnimationCurve.Linear(0, 0, 1, 1);
+
+        public override SequenceActionBehavior CreateBehavior(GameObject owner)
+        {
+            return new CanvasGroupActionBehavior(owner, this);
+        }
+
+        public class CanvasGroupActionBehavior : SequenceActionBehavior
+        {
+            private CanvasGroupActionData data;            private CanvasGroup canvasGroup;            private float timer;            private float baseValue;
+            public CanvasGroupActionBehavior(GameObject owner, CanvasGroupActionData data) : base(owner)
+            {
+                this.data = data;
+            }
+
+            public override void Setup()
+            {
+                canvasGroup = owner.GetComponent<CanvasGroup>();
+            }
+
+            public override IEnumerator Execute()
+            {
+                baseValue = canvasGroup.alpha;
+
+                while (timer < data.Duration)
+                {
+                    timer += Time.deltaTime;
+                    canvasGroup.alpha = Mathf.Lerp(baseValue, data.TargetValue , data.Curve.Evaluate(timer/data.Duration));
+                    yield return null;
+                }
+            }
+        }
+    }
+}
---
+++ Packages/com.justinprat07.sequencer/Runtime/Actions/DelayActionData.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using UnityEngine;
+
+namespace Sequencer.Actions
+{
+    [CreateAssetMenu(fileName = "DelayActionData", menuName = "Sequencer/DelayActionData")]
+    public class DelayActionData : SequenceActionData
+    {
+        public float Value;
+
+        public override SequenceActionBehavior CreateBehavior(GameObject owner)
+        {
+            return new DelayActionBehavior(owner, this);
+        }
+
+        public class DelayActionBehavior : SequenceActionBehavior
+        {
+            private DelayActionData data;            private WaitForSeconds delay;
+            public DelayActionBehavior(GameObject owner, DelayActionData data) : base(owner)
+            {
+                this.data = data;
+            }
+
+            public override void Setup()
+            {
+                delay = new WaitForSeconds(data.Value);
+            }
+
+            public override IEnumerator Execute()
+            {
+                yield return delay;
+            }
+        }
+    }
+}
